71642074c7c42a70bc0f1d981596e1a08edf6cda
Rami Sorikh <ramis@live.ca>
2022-10-07 14:45:49 -0400

    Added GTM

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,27 @@ import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
 
 
+////////////////////////////////////////////////////
+/// START: GOOGLE TAG MANAGER
+////////////////////////////////////////////////////
+// @ts-ignore
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'G-HWSTN8PYQP',
+  // dataLayer: {
+  //     userId: '001',
+  //     userProject: 'project'
+  // }
+}
+
+TagManager.initialize(tagManagerArgs)
+
+////////////////////////////////////////////////////
+/// END: GOOGLE TAG MANAGER
+////////////////////////////////////////////////////
+
+
 const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container)
